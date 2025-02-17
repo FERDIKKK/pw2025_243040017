10efdb7f1417a48b31be790ed3f1382c7e639a7a
FERDIKKK <ikhsanferdiansyah2020@gmail.com>
2025-02-17 09:50:13 +0700

    first commit

---
+++ tugas/tugas2.php
@@ -0,0 +1,9 @@
+<?php
+$x = "17";
+
+echo "Aku adalah angka" . "<b> $x</b>" . "<br>";
+echo "jika aku di kali 5, maka aku sekarang menjadi" . "<b>" . $x * 5 . "</b>" . "<br>";
+echo "jika aku di bagi 2, maka aku sekarang menjadi" . "<b>" . $x / 2 . "</b>" . "<br>";
+echo "jika aku di tambah 75, maka aku sekarang menjadi" . "<b>" . $x + 75 . "</b>" . "<br>";
+echo "jika aku di kurang 20, maka aku sekarang menjadi" . "<b>" . $x - 2 . "</b>" . "<br>";
+?>
