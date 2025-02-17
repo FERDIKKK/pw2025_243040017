6253dd6dd684262d46db0ff709b85ce1a3537ada
FERDIKKK <ikhsanferdiansyah2020@gmail.com>
2025-02-17 20:45:09 +0700

    saya telah mengerjakan tugas pw_243040017

--- tugas/tugas2.php
+++ tugas/tugas2.php
@@ -6,4 +6,5 @@ echo "jika aku di kali 5, maka aku sekarang menjadi" . "<b>" . $x * 5 . "</b>" .
 echo "jika aku di bagi 2, maka aku sekarang menjadi" . "<b>" . $x / 2 . "</b>" . "<br>";
 echo "jika aku di tambah 75, maka aku sekarang menjadi" . "<b>" . $x + 75 . "</b>" . "<br>";
 echo "jika aku di kurang 20, maka aku sekarang menjadi" . "<b>" . $x - 2 . "</b>" . "<br>";
+
 ?>
